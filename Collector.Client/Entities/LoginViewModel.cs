957e1c5050f558447ca298030185a77e66178fcf
Notmisa09 <125936102+Notmisa09@users.noreply.github.com>
2024-11-07 14:45:31 -0400

    LoginScreen Almost done

---
+++ Collector.Client/Entities/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Collector.Client.Entities
+{
+    public class LoginViewModel
+    {
+        public string Email {  get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty ;
+    }
+}
