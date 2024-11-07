5eb8887366a55e4249de24f40f18074e51d6eb7e
edwin vasquez cruz <vasquezedwin954@gmail.com>
2024-11-07 18:59:33 -0400

    Basic structure of the person registration form.

---
+++ Collector.Client/Entities/RegisterUserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace Collector.Client.Entities
+{
+    public class RegisterUserViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string UserName {  get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email {  get; set; } = string.Empty;
+        public string Identification {  get; set; } = string.Empty;
+        public string Phone {  get; set; } = string.Empty;
+
+        [MinLength(8)]
+        public string Password { get; set; } = string.Empty;
+
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public IBrowserFile? ProfileImage { get; set; }
+    }
+}
