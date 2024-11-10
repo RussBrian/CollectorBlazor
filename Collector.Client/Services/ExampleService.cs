1ea875eb659596064a674d4fc165f9e9c642e457
Russel Martinez <russel_martinez@bhd.com.do>
2024-11-10 15:14:11 -0400

    fix: rename example class

--- Collector.Client/Services/ExampleService.cs
+++ Collector.Client/Services/ExampleService.cs
@@ -4,25 +4,19 @@ using Microsoft.Extensions.Options;
 
 namespace Collector.Client.Services
 {
-    public class ExampleService
+    public class ExampleService(HttpClientServiceExtensions httpClientService, IOptions<AppOptions> options)
     {
-        private readonly HttpClientServiceExtensions _httpClientService;
-        private readonly AppOptions _appOptions;
+        private readonly HttpClientServiceExtensions _httpClientService = httpClientService;
+        private readonly AppOptions _appOptions = options.Value;
 
-        public ExampleService(HttpClientServiceExtensions httpClientService, IOptions<AppOptions> options)
-        {
-            _httpClientService = httpClientService;
-            _appOptions = options.Value;
-        }
+        public async Task<object?> GetUser() => await _httpClientService.CustomGetAsync<ReqExample>(_appOptions.UrlExampleService);
+        public async Task<object?> GetUserById(int id) => await _httpClientService.CustomGetAsync<ReqExample>(_appOptions.UrlExampleService, id);
 
-        public async Task<object?> GetUser() => await _httpClientService.CustomGetAsync<User>(_appOptions.UrlExampleService);
-        public async Task<object?> GetUserById(int id) => await _httpClientService.CustomGetAsync<User>(_appOptions.UrlExampleService, id);
-
-        public async Task<ResponseUser?> PostUser(User user)
-            => await _httpClientService.CustomPostAsync<ResponseUser, User>(_appOptions.UrlExampleService, user);
+        public async Task<ResponseExample?> PostUser(ReqExample example)
+            => await _httpClientService.CustomPostAsync<ResponseExample, ReqExample>(_appOptions.UrlExampleService, example);
     }
 
-    public class User
+    public class ReqExample
     {
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
@@ -30,7 +24,7 @@ namespace Collector.Client.Services
         public int Age { get; set; }
     }
 
-    public class ResponseUser
+    public class ResponseExample
     {
         public string Message { get; set; } = string.Empty;
     }
