ee65e86ca9acf3cdeffaabf51185d73c57518230
Russel Martinez <russel_martinez@bhd.com.do>
2024-11-25 01:39:00 -0400

    feat(HttpClientServiceExtensions): Se añade nuevo metodo para consumir las solicitudes PUT (Update)

--- Collector.Client/Services/Volunteer/IVolunteerService.cs
+++ Collector.Client/Services/Volunteer/IVolunteerService.cs
@@ -1,10 +1,18 @@
-﻿using Collector.Client.Dtos.Volunteer;
+﻿using Collector.Client.Dtos;
+using Collector.Client.Dtos.Volunteer;
 
 namespace Collector.Client.Services.Volunteer
 {
     public interface IVolunteerService
     {
+        #region Voluunteer
         Task<ResVolunteerDto?> CreateVolunteer(ReqVolunteerDto request);
         Task<ResVolunteerDto?> RegisterUserInVolunteer(ReqVolunteerDto request);
+        Task<List<ResVolunteerDto>> GetAllVolunteers(PaginationDto pagination);
+        #endregion
+
+        #region User/Volunteer
+
+        #endregion
     }
 }
--- Collector.Client/Services/Volunteer/VolunteerService.cs
+++ Collector.Client/Services/Volunteer/VolunteerService.cs
@@ -1,8 +1,11 @@
-﻿using Collector.Client.Dtos.Response;
+﻿using Collector.Client.Dtos;
+using Collector.Client.Dtos.Response;
 using Collector.Client.Dtos.Volunteer;
+using Collector.Client.Helpers;
 using Collector.Client.Utilities.Extensions;
 using Collector.Client.Utilities.Options;
 using Microsoft.Extensions.Options;
+using static MudBlazor.CategoryTypes;
 
 namespace Collector.Client.Services.Volunteer
 {
@@ -24,5 +27,20 @@ namespace Collector.Client.Services.Volunteer
 
         public async Task<ResVolunteerDto?> RegisterUserInVolunteer(ReqVolunteerDto request)
             => await _httpExtension.CustomFormDataAsync<ResVolunteerDto, ReqVolunteerDto>(_appOptions.UrlVolunteerService, request);
+
+        public async Task<List<ResVolunteerDto>> GetAllVolunteers(PaginationDto pagination)
+        {
+            var volunteers = await _httpExtension.CustomGetAsync<Response<List<ResVolunteerDto>>>(_appOptions.UrlVolunteerService);
+
+            var result = volunteers as Response<List<ResVolunteerDto>>;
+
+            if(result?.Value?.Count != 0)
+            {
+                var queryableList = result?.Value?.AsQueryable();
+                return [.. queryableList?.Pagination(pagination)];
+            }
+
+            return [];
+        }
     }
 }
