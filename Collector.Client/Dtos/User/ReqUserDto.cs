21d325fff877878ffca3e4e30179b45d724b3af0
edwin vasquez cruz <vasquezedwin954@gmail.com>
2024-11-30 01:45:14 -0400

    refactor: Image requesting format.

--- Collector.Client/Dtos/User/ReqUserDto.cs
+++ Collector.Client/Dtos/User/ReqUserDto.cs
@@ -10,12 +10,12 @@ namespace Collector.Client.Dtos.Login
         public string? Phone { get; set; }
         public string? Email { get; set; }
         public string? Identification { get; set; }
-        public IBrowserFile? File { get; set; }
-        public string? Image { get; set; } = "DEFAULT";
+        public string File { get; set; } = string.Empty;
+        public string? Image { get; set; }
         public string Gender { get; set; } = "M";
         public string Address { get; set; } = "DEFAULT";
         public string? Password { get; set; }
-        public int RolId { get; set; } = 8;
+        public int RolId { get; set; }
         public bool IsMobileRegister { get; set; } = false;
         public string AddedBy { get; set; } = "ADMIN";
     } 
