1a3bf6ede8d40978caacf0fb438bf19f46b7c615
Notmisa09 <125936102+Notmisa09@users.noreply.github.com>
2024-12-03 16:55:40 -0400

    feat : GetDetails report && volunteer.

---
+++ Collector.Client/Dtos/Pages/PagesDto.cs
@@ -0,0 +1,27 @@
+﻿namespace Collector.Client.Dtos.Pages
+{
+    public class PagesDto
+    {
+        public string Text { get; set; }
+        public int Page {  get; set; }
+        public bool Available { get; set; } = true;
+        public bool Active { get; set; } = false;
+
+        public PagesDto (int pagina) { 
+            Page = pagina;
+            Available = true;
+        }
+        public PagesDto(int pagem, bool available)
+        {
+            Page = pagem;
+            Available = available;
+        }
+
+        public PagesDto (int pagem, bool available, string text) 
+        {
+            Page = pagem;
+            Available = available;
+            Text = text;
+        }            
+    }
+}
