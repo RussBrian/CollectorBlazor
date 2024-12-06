75ae1cc9e2acb7a3726ab764a24fdf1edf2acfef
Notmisa09 <125936102+Notmisa09@users.noreply.github.com>
2024-12-06 16:07:43 -0400

    Changes on Validate email (Forgort Password)

---
+++ Collector.Client/Validations/ResVolunteerDtoValidation.cs
@@ -0,0 +1,16 @@
+﻿using Collector.Client.Dtos.Volunteer;
+using FluentValidation;
+
+namespace Collector.Client.Validations
+{
+
+    public class ResVolunteerDtoValidation : AbstractValidator<ResVolunteerDto>
+    {
+        public ResVolunteerDtoValidation()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Este campo no puede estar vacio.");
+            RuleFor(x => x.Details).NotEmpty().WithMessage("Este campo no puede estar vacio.");
+            RuleFor(x => x.VolunteerDate).NotEmpty().WithMessage("Este campo no puede estar vacio.");
+        }
+    }
+}
