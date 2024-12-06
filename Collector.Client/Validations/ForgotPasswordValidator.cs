fcedc3a165d1570d5459f4468d9f062efc35cc7d
Notmisa09 <125936102+Notmisa09@users.noreply.github.com>
2024-12-06 03:19:11 -0400

    Cascade Validation for ForgotPassword

--- Collector.Client/Validations/ForgotPasswordValidator.cs
+++ Collector.Client/Validations/ForgotPasswordValidator.cs
@@ -10,6 +10,7 @@ namespace Collector.Client.Validations
             RuleSet("Step1", () =>
             {
             RuleFor(e => e.Email)
+                            .Cascade(CascadeMode.Stop)
                 .NotEmpty().WithMessage("El correo electrónico no puede estar vacio.")
                 .EmailAddress().WithMessage("El correo electrónco debe tener un formato valido.");
             });
@@ -17,6 +18,7 @@ namespace Collector.Client.Validations
             RuleSet("Step2", () =>
             {
                 RuleFor(c => c.Code)
+                            .Cascade(CascadeMode.Stop)
                .NotEmpty().WithMessage("El código de verificación no puede estar vacío.")
                .GreaterThan(0).WithMessage("El código de verificación debe ser válido")
                .InclusiveBetween(100000, 999999).WithMessage("El código debe ser exactamente 6 dígitos numéricos.");
@@ -25,12 +27,13 @@ namespace Collector.Client.Validations
             RuleSet("Step3", () =>
             {
                 RuleFor(p => p.NewPassword)
-                 .Cascade(CascadeMode.Stop)
-                 .NotEmpty().WithMessage("Debe de digitar una contraseña.")
-                 .Matches(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,16}$")
-                    .WithMessage("La contraseña debe de estar en un formato valido.");
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty().WithMessage("Debe de digitar una contraseña.")
+                .Matches(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,16}$")
+                   .WithMessage("La contraseña debe de estar en un formato valido.");
 
             RuleFor(cp => cp.ConfirmPassword)
+                .Cascade(CascadeMode.Stop)
                 .Matches(cp => cp.NewPassword).WithMessage("Ambas contraseñas deben de conincidir.");
             });
         }
